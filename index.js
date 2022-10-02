6c9b65cbd84191498108a47ce26413d76476ddb4
ElyasBelkhir <belkhirelyas@gmail.com>
2022-10-02 08:28:02 -0500

    yo?

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
 
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
@@ -22,13 +18,6 @@ for (const file of eventFiles) {
 	}
 }
 
-for (const file of commandFiles) {
-	
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.data.name, command);
-
-}
 
 
 client.login(process.env.TOKEN);
