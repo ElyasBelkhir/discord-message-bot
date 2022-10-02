7627ab87c6230b42b7caa113e9b2ac9653b85a99
ElyasBelkhir <belkhirelyas@gmail.com>
2022-10-02 08:07:30 -0500

    yo

--- index.js
+++ index.js
@@ -21,6 +21,19 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 
 client.login(process.env.TOKEN);
