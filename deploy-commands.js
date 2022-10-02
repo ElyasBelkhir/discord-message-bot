d8d8496165db3e06958d997eeb3747354bd44c44
ElyasBelkhir <belkhirelyas@gmail.com>
2022-10-02 17:45:21 -0500

    heroku fix

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,19 +2,17 @@ const { SlashCommandBuilder, Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { clientId, guildId, token } = require('./config.json');
 require("dotenv").config();
+const fs = require('node:fs');
+const path = require('node:path');
 
 
-const commands = [
-	new SlashCommandBuilder().setName('yo').setDescription('Replies with yo!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-    new SlashCommandBuilder().setName('meow').setDescription('Replies with meow'),
 
-]
-	.map(command => command.toJSON());
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-const rest = new REST({ version: '10' }).setToken(token);
-
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
-	.catch(console.error);
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
